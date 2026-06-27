3d9b5ad56c51612c79dbb42f88dee42a66d32ee1
Jardel Santos <jardelsantos78@hotmail.com>
2026-06-27 09:34:48 -0300

    Add files via upload

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,6 +1,6 @@
 // Insira aqui a URL gerada após publicar o seu script como "Aplicativo da Web"
 //const APPS_SCRIPT_URL = "https://google-drive-shield.jardelassis.workers.dev/";
-const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxdbi7MyAHY8i5rrhjhZ_OpL8NP498E5hqZYlQEvCVvJzoM2CCR4LMb592aCRDoqbmr/exec"
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxdbi7MyAHY8i5rrhjhZ_OpL8NP498E5hqZYlQEvCVvJzoM2CCR4LMb592aCRDoqbmr/exec";
 
 // Estados de controle baseados nas regras de negócio do seu arquivo GS
 let loginEtapa = "CREDENCIAIS"; // Pode mudar para "MFA" ou "ALTERAR_SENHA"
--- assets/js/viewer.js
+++ assets/js/viewer.js
@@ -1,6 +1,6 @@
     // URL Ocultada na arquitetura - o front consome o token guardado no login
 	//const APPS_SCRIPT_URL = "https://google-drive-shield.jardelassis.workers.dev/";
-	const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxdbi7MyAHY8i5rrhjhZ_OpL8NP498E5hqZYlQEvCVvJzoM2CCR4LMb592aCRDoqbmr/exec"
+	const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxdbi7MyAHY8i5rrhjhZ_OpL8NP498E5hqZYlQEvCVvJzoM2CCR4LMb592aCRDoqbmr/exec";
 
     // Mesma chave usada pelo login.html ao salvar a sessão (sessionStorage.setItem("portalToken", ...))
     const TOKEN = sessionStorage.getItem("portalToken");
